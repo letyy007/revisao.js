5537e958c41c4e171b2e3bbaeeb3b265f86380f4
lelety2007 <leticiagrazielle258@gmail.com>
2025-03-25 19:06:34 +0000

    frase

--- variaveis.js
+++ variaveis.js
@@ -3,7 +3,7 @@ let idade = 18
 const signo = "peixes"
 
 /*  i
-var = variavel que muda o valor na execução do codigo
-let = variavel que muda o valor na execução do codigo
+var = variavel que muda o valor na execução do codigo 
+let = variavel que muda o valor na execução do codigo e não pode ter duas variaveis com o mesmo nome
 cons = variavel constante não muda o valor da execução
 */
