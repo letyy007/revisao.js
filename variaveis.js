e182b631abbc567fbdf524a360a17193177d80d3
lelety2007 <leticiagrazielle258@gmail.com>
2025-03-25 19:03:49 +0000

    frase

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+var nome = "lety" 
+let idade = 18
+const signo = "peixes"
+
+/*  i
+var = variavel que muda o valor na execução do codigo
+let = variavel que muda o valor na execução do codigo
+cons = variavel constante não muda o valor da execução
+*/
